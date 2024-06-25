365492f9369a0f789112334a8828dc7234210b4a
DlinaNogka <artem.vasiutin@nure.ua>
2024-06-25 21:03:18 +0300

    +db;+User_db

--- Database/ApplicationDbContext.cs
+++ Database/ApplicationDbContext.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BidDecore.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BidDecore.Database
 {
     public class ApplicationDbContext : DbContext 
     {
+        public DbSet<User> Users { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
             
